af95b5b9fca351851075814cd732b12a336fad74
travisjn <tdizzletrav@gmail.com>
2015-03-15 16:00:27 -0700

    Add explosion effect on collision

--- watchout.js
+++ watchout.js
@@ -1,32 +1,38 @@
 // start slingin' some d3 here.
 
+var settings = {
+  height : 300,
+  width : 300,
+  enemyR : 7,
+  playerR : 7,
+  numEnemies : 10,
+}
+
 var Enemy = function(x, y){
   this.x = x;
   this.y = y;
-  this.radius = 10;
   this.color = "red";
 }
 
 var Player = function(x, y){
   this.x = x;
   this.y = y;
-  this.radius = 7;
   this.color = "red";
 }
 
 //Loop through number of enemies and populate a new enemy at a random x and y coordinate
-var populateEnemies = function(numEnemies){
+var populateEnemies = function(){
   var x;//random x
   var y;//enemies start off the screen then drop in
-  for (var i = 0; i < numEnemies; i++){
-    x = 0 - (Math.floor(Math.random() * (1000)));
-    y = 0 - (Math.floor(Math.random() * (1000)));
+  for (var i = 0; i < settings.numEnemies; i++){
+    x = 0 - (Math.floor(Math.random() * (settings.width * 2)));
+    y = 0 - (Math.floor(Math.random() * (settings.height * 2)));
     x *= (Math.round(Math.random()) * 2 - 1);
     y *= (Math.round(Math.random()) * 2 - 1);
-    if (x > 0 && x < 500){
+    if (x > 0 && x < settings.width){
       x *= -1;
     }
-    if (y > 0 && y < 500){
+    if (y > 0 && y < settings.height){
       y *= -1;
     }
     var newEnemy = new Enemy(x, y);
@@ -37,7 +43,7 @@ var populateEnemies = function(numEnemies){
   svg.selectAll('circle').data(enemies).enter()
       .append('circle')
       .attr('class', 'enemy')
-      .attr("r", function(d, i){ return d.radius; })
+      .attr("r", settings.enemyR + "px")
       .attr("cx", function(d, i){ return d.x; })
       .attr("cy", function(d, i){ return d.y; })
       .attr('fill', 'black');
@@ -46,25 +52,25 @@ var populateEnemies = function(numEnemies){
   svg.selectAll('.enemy').data(enemies)
       .transition().ease('elastic').duration(1500)
       .attr("cy", function(d, i){ 
-        d.y = Math.floor(Math.random() * (300 - 50) + 50);
+        d.y = Math.floor(Math.random() * (settings.height - 50) + 50);
         return d.y;
       })
       .attr("cx", function(d, i){ 
-        d.x = Math.floor(Math.random() * (300 - 50) + 50);
+        d.x = Math.floor(Math.random() * (settings.width - 50) + 50);
         return d.x;
       });
 }
 
 //Create the player
 var populatePlayer = function(){
-  var player = new Player(330, 100);  //Create the player
+  var player = new Player(settings.width - 25, 25);  //Create the player
   playerArray.push(player);  //Push the player into a global array to easily access with D3
 
   //Print to screen
   svg.selectAll('.player').data(playerArray).enter()
      .append('circle')
      .attr('class', 'player')
-     .attr("r", function(d, i){ return d.radius; })
+     .attr("r", settings.playerR)
      .attr("cx", function(d, i){ return d.x; })
      .attr("cy", function(d, i){ return d.y; })
      .attr('fill', function(d, i){ return d.color; });
@@ -80,13 +86,13 @@ var moveEnemies = function() {
   svg.selectAll('.enemy').data(enemies)
      .transition().duration(enemyMovementSpeed)
      .attr('cx', function(d, i){ 
-        startX = d.x;
-        d.x = Math.floor(Math.random() * (300 - 50) + 50); 
+        // startX = d.x;
+        d.x = Math.floor(Math.random() * ((settings.width - settings.enemyR) - 50) + 50); 
         return d.x;
       })
      .attr('cy', function(d, i){ 
-        startY = d.y;
-        d.y = Math.floor(Math.random() * (300 - 50) + 50); 
+        // startY = d.y;
+        d.y = Math.floor(Math.random() * ((settings.height - settings.enemyR) - 50) + 50); 
         return d.y;
       })
      .tween('Collision Check', function(d, i){  //Run checkCollision at each step in the transition to register collisions while transitioning
@@ -97,7 +103,6 @@ var moveEnemies = function() {
         if (checkCollision(d.x, d.y) === true){  
           collided = true;
           score = 0;
-          collisions++;
        }
      }
      }
@@ -109,7 +114,6 @@ var checkCollision = function(targetX, targetY){
   var player = playerArray[0];
   var playerX = player.x,
       playerY = player.y;
-  var enemyRadius = enemies[0].radius;
 
   //get distances between objects
   var dx = targetX - player.x;
@@ -117,8 +121,9 @@ var checkCollision = function(targetX, targetY){
   var distance = Math.sqrt(dx * dx + dy * dy);
 
   //check if the objects are close enough to be touching
-  if (distance <= player.radius + enemyRadius){
+  if (distance <= settings.playerR + settings.enemyR){
     if (playerDead === false){
+      collisions++;
       screenShake();
       killPlayer();
     }
@@ -131,35 +136,42 @@ var checkCollision = function(targetX, targetY){
 
 var screenShake = function() {
   if (playerDead === false){
-  var gameScreen = d3.select('.gameSpace');
+    gameScreen.transition().duration(50).style({
+      'top' : '7px',
+      'left' : '7px'
+    }).transition().duration(50).style({
+      'top' : '0px',
+      'left' : '0px'
+    }).transition().duration(50).style({
+      'top' : '7px',
+      'left' : '7px'
+    }).transition().duration(50).style({
+      'top' : '-7px',
+      'left' : '-7px'
+    }).transition().duration(50).style({
+      'top' : '7px',
+      'left' : '7px'
+    }).transition().duration(50).style({
+      'top' : '-7px',
+      'left' : '-7px'
+    });
+    //Create explosion circle at player coordinates
+    var explosionCircle = svg.selectAll('.collisionCircle').data([1]).enter()
+      .append('circle').attr('class', '.collisionCircle')
+      .attr('cx', playerArray[0].x).attr('cy', playerArray[0].y)
+      .attr('r', settings.playerR).attr('fill', 'red');
 
-  gameScreen.transition().duration(50).style({
-    'top' : '7px',
-    'left' : '7px'
-  }).transition().duration(50).style({
-    'top' : '-7px',
-    'left' : '-7px'
-  }).transition().duration(50).style({
-    'top' : '7px',
-    'left' : '7px'
-  }).transition().duration(50).style({
-    'top' : '-7px',
-    'left' : '-7px'
-  }).transition().duration(50).style({
-    'top' : '7px',
-    'left' : '7px'
-  }).transition().duration(50).style({
-    'top' : '-7px',
-    'left' : '-7px'
-  });
-}
+    explosionCircle.transition().duration(350)
+      .style('opacity', 0).attr('r', 75).attr('fill', 'orange').remove();
+
+  }
 }
 
 var killPlayer = function(){
   svg.select('.player').data(playerArray)
     .transition().duration(1000).ease('bounce')
-  .attr('cy', '493px').each('end', function(d){
-    d.y = 493;
+  .attr('cy', settings.height - settings.playerR).each('end', function(d){
+    d.y = settings.height - settings.playerR;
   });
   playerDead = true;
 }
@@ -167,9 +179,9 @@ var killPlayer = function(){
 //Called at an interval, updates the scores displayed
 var updateScore = function(collided) {
 
-  if (collided === true && score > 1){
+  if (collided === true && playerDead === false){
     score = 0;
-    collisions++;    
+    // collisions++;    
   } else if (collided === false && playerDead === false){
     score++;  //Increase the score at each interval
   }
@@ -184,12 +196,26 @@ var printScore = function(){
     .text(function(d){ return d });
 }
 
-
 //----GLOBAL VARIABLES----
-  var gameWidth = 500;
-  var gameHeight = 300;
   d3.select('.gameSpace').append("svg");
-  var svg = d3.select('svg').attr("height", 500).attr("width", 500);
+  var svg = d3.select('svg').attr("height", settings.height).attr("width", settings.width).attr('fill', 'url(#background.jpg)');
+  var gameScreen = d3.select('.gameSpace').style("height", settings.height + "px").style("width", settings.width + "px");
+
+  //draw background
+  // d3.select('.gameSpace').selectAll('defs').data([1]).append('defs');
+  // d3.select('defs').selectAll('pattern').data([1]).enter().append('pattern');
+  // d3.select('pattern')
+  //     .attr('id', 'background')
+  //     .attr('patternUnits', 'userSpaceOnUse')
+  //     .attr('x', 0).attr('y', 0)
+  //     .attr('width', settings.width).attr('height', settings.height);
+  // d3.select('pattern').selectAll('image').data(["background.jpg"]).enter()
+  //     .attr('href', function(d){ return d; })
+  //     .attr('width', settings.width).attr('height', settings.height)
+  //     .append('image');
+  // d3.select('svg').attr('fill', 'url(#background)');
+
+
   var enemies = [];
   var playerArray = [];  //d3 accepts arrays as data arguments so push the player object into an array even though there is only one of them
   var score = 0;
@@ -203,9 +229,8 @@ var startGame = function() {
   //add event listener for clicking and dragging the player
   
   var mouseCoordinates = [0, 0];     //d3 coordinates are stored in an array, [x, y]
-  var numEnemies = 10;               //Number of enemies
   var timeBetweenEnemyMoves = 1000;  //Time between each time the enemies move to a new location in ms
-  populateEnemies(numEnemies); 
+  populateEnemies(settings.numEnemies); 
   populatePlayer();                  //Create the player and paint to screen
   var player = playerArray[0];
 
@@ -219,19 +244,13 @@ var startGame = function() {
       player.x = mouseCoordinates[0];
       player.y = mouseCoordinates[1];
       d3.selectAll('.player').attr('cx', player.x).attr('cy', player.y);
-    } else {
-      d3.selectAll('.player').data(playerArray).on('click', function(d){
-        playerDead = false;
-      })
-
     }
-    // else {
-    //   d3.selectAll('.player').transition().duration(500).attr('cx', player.x).attr('cy', player.y);
-    //   setTimeout(function(){ playerDead = false; }, 1000);
-    //   // playerDead = false;
-    // }
   });  
+  d3.selectAll('.player').data(playerArray).on('click', function(d){
+      playerDead = false;
+  });
+
+  
 }
 
 startGame();
-//d3.timer(checkCollision);
